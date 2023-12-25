ad98bec987472a46266b5733b0a68006f3c4a8cd
marlonmp <marlonmp214@protonmail.com>
2023-12-25 11:13:31 -0500

    tag validators added

---
+++ app/api/tags/validators.js
@@ -0,0 +1,21 @@
+import { z } from 'zod';
+
+import prisma from '@/lib/prisma';
+import { clearString } from '@/lib/utils';
+
+export const tagIdValidator = z.string()
+  .uuid()
+  .transform(async id => await prisma.tag.findUniqueOrThrow({ where: { id }}));
+
+const name = z.string()
+  .max(32)
+  .trim()
+  .transform(val => val.replace(/\ {1,}/g, '_'))
+  .transform(val => clearString(val))
+  .transform(val => val.toLowerCase());
+
+export const tagFilterValidator = z.object({ name__sh: name });
+
+export const tagCreateValidator = z.object({ name });
+
+export const tagUpdateValidator = z.object({ name });
