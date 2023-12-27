8cd0f460705c92815fb0e1810890f45a7d34eeee
marlonmp <marlonmp214@protonmail.com>
2023-12-26 23:28:31 -0500

    transaction list and create endpoinsst modified

--- app/api/transactions/route.js
+++ app/api/transactions/route.js
@@ -1,40 +1,51 @@
 import prisma from '@/lib/prisma';
 import withErrorHandler from '@/lib/error.handler';
+import { getFilters, getOrdering, getPagination } from '@/lib/filters';
 
 import { transactionCreateValidator, transactionFilterValidator } from './validators';
-import { getFilters, getOrdering, getPagination } from '@/lib/filters';
+import { transactionMapper } from './mappers';
 
 export const GET = withErrorHandler(async function (req) {
   const pagination = getPagination(req);
   const filters = await getFilters(req, transactionFilterValidator);
   const ordering = getOrdering(req, ['date']);
 
+  const count = await prisma.transaction.count(filters);
+
   const transactions = await prisma.transaction.findMany({
     ...pagination, ...ordering, ...filters,
-    include: {
-      tags: {
-        include: {
-          tag: { select: { id: true, name: true } }
-        }
-      }
-    }
+    include: { tags: { include: { tag: true } } },
   });
 
-  return Response.json({
-    count: await prisma.transaction.count(filters),
-    data: [...transactions.map(transaction => ({
-      ...transaction,
-      tags: [...transaction.tags.map(tag => ({ id: tag.tag_id, name: tag.tag.name, added_at: tag.added_at }))]
-    }))],
-  });
+  const data = transactions.map(transactionMapper);
+
+  return Response.json({ count, data });
 });
 
 export const POST = withErrorHandler(async function (req) {
   const jsonData = await req.json();
 
-  const data = await transactionCreateValidator.parseAsync(jsonData);
+  const { tags, ...validatedData } = await transactionCreateValidator.parseAsync(jsonData);
 
-  const transaction = await prisma.transaction.create({ data });
+  const prismaTransaction = async tx => {
+    const transaction = await tx.transaction.create({
+      data: validatedData,
+      include: { tags: { include: { tag: true } } },
+    });
 
-  return Response.json({data: transaction}, {status:201});
+    await tx.transaction_tag.createMany({
+      data: [
+        ...tags.map(tag => ({ transaction_id: transaction.id, tag_id: tag.id })),
+      ],
+      skipDuplicates: true,
+    });
+
+    return transaction;
+  };
+
+  const transaction = await prisma.$transaction(prismaTransaction);
+
+  const data = transactionMapper({ ...transaction, tags,  });
+
+  return Response.json({ data }, {status:201});
 });
