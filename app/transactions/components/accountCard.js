eb3e39b1f6840223dc2d28e181ba9bb5a8cd23dd
marlonmp <marlonmp214@protonmail.com>
2023-12-28 21:38:58 -0500

    account card style changed

--- app/transactions/components/accountCard.js
+++ app/transactions/components/accountCard.js
@@ -1,15 +1,23 @@
+'use client';
+
+import { currencyMapper, dateTimeMapper } from '@/lib/mappers';
 import styles from '@/styles/account-card.module.css';
 
-export function AccountCard({ active = false }) {
+/**
+ * Account card
+ * @param {{ account, active: boolean = false, tabIndex?: number }} props
+ * @returns {JSX.Element}
+ */
+export default function AccountCard({ account, active = false, tabIndex, onClick }) {
   const activeClass = active ? styles.active : '';
   const className = `${styles.accountCard} ${activeClass}`;
 
   return (
-    <div className={className}>
-      <div className='font-bold text-xl'>Account name</div>
-      <div className='text-md'>Account type</div>
-      <div className='self-end mt-4 font-black text-lg text-slate-200'>$ 10,000,000.00</div>
-      <div className='self-end text-xs text-slate-600'>Last update at: 12/12/2023 12:00 pm</div>
+    <div className={className} tabIndex={tabIndex} onClick={onClick}>
+      <div className='font-bold text-xl'>{account.name}</div>
+      <div className='text-md'>{account.type}</div>
+      <div className='self-end mt-4 font-black text-lg text-slate-200'>{currencyMapper(account.balance)}</div>
+      <div className='self-end text-xs text-slate-600'>Last update at: {dateTimeMapper(account.updated_at)}</div>
     </div>
   );
 }
