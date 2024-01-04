f1b3522f92b19bfcb31895e55b67633738d447c1
marlonmp <marlonmp214@protonmail.com>
2024-01-04 01:39:08 -0500

    maintain components added to account modal

--- app/transactions/components/accountModal.js
+++ app/transactions/components/accountModal.js
@@ -1,17 +1,56 @@
-import React from 'react';
+import { useForm } from '@mantine/form';
+import { Modal, Button, Box, TextInput, Select } from '@mantine/core';
+import { IconCircleX, IconId, IconPig, IconSend2, IconWallet } from '@tabler/icons-react';
+
+const initialAccountValues = { name: '', type: null };
+
+const accountTypes = [
+  { value: 'savings', label: 'Savings' },
+  { value: 'credit', label: 'Credit' },
+];
 
 /**
  * This component is a form to create or update accounts
  * @param {{
- *  open: boolean = false,
+ *  opened: boolean = false,
  *  type: 'create' | 'update' |'retrieve' = 'create',
  *  account?: object,
  *  onClose?: () => void,
  *  onSubmit?: (account: object) => void
  * }} props
  */
-export default function accountModal({ open = false, type = 'create', account, onClose, onSubmit }) {
+export default function AccountModal({ opened = false, type = 'create', account, onClose, onSubmit }) {
+  const title = (
+    <div className='flex flex-row flex-nowrap items-center gap-x-2'>
+      <IconWallet />
+      <div>Create account</div>
+    </div>
+  );
+
+  const form = useForm({
+    initialValues: { ...(account || initialAccountValues) },
+  });
+
   return (
-    <>account modal</>
+    <>
+      <Modal centered opened={opened} onClose={onClose} title={title}>
+        <Box max={340}>
+          <form onSubmit={onSubmit} className='flex flex-col gap-y-4'>
+            <TextInput withAsterisk required label='Name' placeholder='Bancolombia' leftSection={<IconId size={14} />}
+              {...form.getInputProps('name')} />
+
+            <Select withAsterisk required clearable label='Type' placeholder='Savings'
+              leftSection={<IconPig size={14} />} data={accountTypes} {...form.getInputProps('type')} />
+
+            <div className='w-full flex flex-row flex-nowrap items-center gap-x-2 mt-3'>
+
+              <Button type='submit' color='green' leftSection={<IconSend2 />}>Submit</Button>
+
+              <Button color='red' leftSection={<IconCircleX />} onClick={onClose}>Cancel</Button>
+            </div>
+          </form>
+        </Box>
+      </Modal>
+    </>
   );
 }
