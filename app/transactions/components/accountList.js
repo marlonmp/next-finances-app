68f47520e604b8769823abc627287189b39ad4f2
marlonmp <marlonmp214@protonmail.com>
2024-01-09 21:18:48 -0500

    accont modal moved to account list component

--- app/transactions/components/accountList.js
+++ app/transactions/components/accountList.js
@@ -1,14 +1,31 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+
+import { useRouter } from 'next/navigation';
+
+import { useDisclosure } from '@mantine/hooks';
+import { Button } from '@mantine/core';
+
 import AccountCard, { AccountCardSkeleton } from './accountCard';
+import AccountModal from './accountModal';
+import { IconPlus } from '@tabler/icons-react';
+
 
 export function AccountListSkeleton() {
   return (
-    <div className='w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
-      <AccountCardSkeleton />
-      <AccountCardSkeleton />
-      <AccountCardSkeleton />
+    <div className='w-full'>
+      <div className='w-full mb-6 flex flex-row justify-between items-center'>
+        <div className='w-20 h-4 font-bold rounded-md bg-slate-600'></div>
+
+        <div className='w-36 h-8 rounded-md bg-slate-700'></div>
+      </div>
+      <div className='w-full max-h-[340px] overflow-y-hidden grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+        <AccountCardSkeleton />
+        <AccountCardSkeleton />
+        <AccountCardSkeleton />
+        <AccountCardSkeleton />
+      </div>
     </div>
   );
 };
@@ -27,6 +44,14 @@ const emptyList = (
 
 export default function AccountList() {
   const [accounts, setAccounts] = useState(null);
+  const router = useRouter();
+
+  const [accountModalOpened, accountModal] = useDisclosure(false);
+
+  function onSubmit(account) {
+    const params = new URLSearchParams({ account_id: account.id });
+    router.push(`transactions?${params}`);
+  }
 
   useEffect(() => { getAccounts().then(accounts => setAccounts(accounts)); }, []);
 
@@ -35,10 +60,20 @@ export default function AccountList() {
   }
 
   return (
-    <div className='w-full max-h-80 pr-1 overflow-y-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
-      {!accounts?.length ?
-        emptyList :
-        accounts?.map((account, i) => <AccountCard key={account.id} tabIndex={i + 1} account={account} />)}
+    <div className='w-full'>
+      <div className='w-full mb-6 flex flex-row justify-between items-center'>
+        <div className=' text-md'>Accounts</div>
+
+        <Button color='green' leftSection={<IconPlus />} onClick={accountModal.open}>Add account</Button>
+      </div>
+
+      <div className='w-full max-h-[340px] pr-1 overflow-y-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+        {!accounts?.length ?
+          emptyList :
+          accounts?.map((account, i) => <AccountCard key={account.id} tabIndex={i + 1} account={account} />)}
+      </div>
+
+      <AccountModal opened={accountModalOpened} onClose={accountModal.close} onSubmit={onSubmit} />
     </div>
   );
 }
--- app/transactions/page.js
+++ app/transactions/page.js
@@ -1,38 +1,17 @@
 'use client';
 
 import { Button } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
 import { IconPlus } from '@tabler/icons-react';
 
 import AccountList from './components/accountList';
 import TransactionList from './components/transactionList';
 
-import AccountModal from './components/accountModal';
-import { useRouter } from 'next/navigation';
-
 export default function Transactions({ searchParams }) {
   const { account_id } = searchParams || {};
 
-  const router = useRouter();
-
-  const [accountModalOpened, accountModal] = useDisclosure(false);
-
-  function onSubmit(account) {
-    const params = new URLSearchParams({ account_id: account.id });
-    router.push(`transactions?${params}`);
-  }
-
   return (
     <div className='flex flex-col items-center gap-8'>
-      <div className='w-full'>
-        <div className='w-full mb-6 flex flex-row justify-between items-center'>
-          <div className=' text-md'>Accounts</div>
-
-          <Button color='green' leftSection={<IconPlus />} onClick={accountModal.open}>Add account</Button>
-        </div>
-
-        <AccountList key={'accountlist'} />
-      </div>
+      <AccountList key={'accountlist'} />
 
       <div className='w-full'>
         <div className='w-full mb-6 flex flex-row justify-between items-center'>
@@ -43,8 +22,6 @@ export default function Transactions({ searchParams }) {
 
         <TransactionList account_id={account_id} />
       </div>
-
-      <AccountModal opened={accountModalOpened} onClose={accountModal.close} onSubmit={onSubmit} />
     </div>
   );
 }
